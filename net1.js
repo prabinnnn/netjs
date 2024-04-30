288f043f011906ee79f7ccbc42c80fbbb3472a93
prabinnnn <prabinbhandari120@gmail.com>
2024-04-30 18:49:51 +0545

    add net1

--- net1.js
+++ net1.js
@@ -1,4 +1,17 @@
-const roleA = ["admin", "manger"];
-const roleB = ["user", "recepitation"];
-const newRole = roleA.some((role) => roleB.includes(role));
-console.log(newRole);
+// const roleA = ["admin", "manger"];
+// const roleB = ["user", "recepitation"];
+// const newRole = roleA.some((role) => roleB.includes(role));
+// console.log(newRole);
+javascript;
+
+const people = [
+  { name: "prabin", age: 30 },
+  { name: "rabin", age: 20 },
+  { name: "abin", age: 50 },
+];
+
+const checkage = people.sort((a, b) => {
+  return a.age - b.age;
+});
+
+console.log(checkage);
